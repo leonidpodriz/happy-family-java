a2fdb6d6db34687f335c972804fecc2cb65a0832
Leonid Podriz <leonidpodriz@gmail.com>
2021-04-04 13:09:14 +0300

    create Woman constructors

--- src/main/java/org/danit/family/human/Woman.java
+++ src/main/java/org/danit/family/human/Woman.java
@@ -1,6 +1,14 @@
 package org.danit.family.human;
 
 public class Woman extends Human {
+    public Woman(String womanName, String surname, int year) {
+        super(womanName, surname, year);
+    }
+
+    public Woman() {
+        super();
+    }
+
     public void makeup() {
         System.out.print("makeup");
     }
