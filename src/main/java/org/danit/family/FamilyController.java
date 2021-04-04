cdd0fd8799bca0d31f2778369f7b5fc33ef84079
Leonid Podriz <leonidpodriz@gmail.com>
2021-04-04 15:04:23 +0300

    clean up the controller

--- src/main/java/org/danit/family/FamilyController.java
+++ src/main/java/org/danit/family/FamilyController.java
@@ -1,27 +1,7 @@
 package org.danit.family;
 
-import org.apache.commons.lang.NotImplementedException;
-
-public class FamilyController<Request, Response> {
+public class FamilyController {
     private final FamilyService service = new FamilyService(new FamilyDao());
 
-    public Response get(Request request) {
-        throw new NotImplementedException("Here you can do some staff using access to the service");
-    }
 
-    public Response post(Request request) {
-        throw new NotImplementedException("Here you can do some staff using access to the service");
-    }
-
-    public Response put(Request request) {
-        throw new NotImplementedException("Here you can do some staff using access to the service");
-    }
-
-    public Response update(Request request) {
-        throw new NotImplementedException("Here you can do some staff using access to the service");
-    }
-
-    public Response delete(Request request) {
-        throw new NotImplementedException("Here you can do some staff using access to the service");
-    }
 }
